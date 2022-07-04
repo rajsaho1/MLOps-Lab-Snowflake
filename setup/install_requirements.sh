0adc9eafe8d1e40e767165e31933a8abaa8bcbd7
rajsaho1 <44056891+rajsaho1@users.noreply.github.com>
2022-07-04 14:23:56 +0530

    Update install_requirements.sh

--- setup/install_requirements.sh
+++ setup/install_requirements.sh
@@ -3,3 +3,5 @@ python --version
 pip install --upgrade azureml-sdk
 pip install -r requirements.txt
 pip install --upgrade azure-cli
+pip install snowflake-connector-python
+pip install snowflake-snowpark-python
